e8dafed214499b537452781c8e97d04f8bdb3e2c
Máté Mustó <mustomate7@gmail.com>
2023-11-23 16:56:08 +0100

    Update Controller, Create html templates

---
+++ src/main/java/com/example/mindentudas/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.mindentudas;
+
+import org.springframework.data.repository.CrudRepository;
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
---
+++ src/main/java/com/example/mindentudas/WebSecurityConfig.java
@@ -0,0 +1,41 @@
+package com.example.mindentudas;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, proxyTargetClass = true)
+public class WebSecurityConfig {
+    @Autowired
+    private UserDetailsService customUserDetailsService;
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .requestMatchers("/resources/**", "/").permitAll()
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .requestMatchers("/resources/**", "/", "/register", "/regisztral_feldolgoz").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().defaultSuccessUrl("/home").permitAll()
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/").permitAll()
+                .and()
+                .exceptionHandling();
+        return http.build();
+    }
+}
