e7fbc8c6807ba6e43ce5957f3c204a7676c407be
Máté Mustó <mustomate7@gmail.com>
2023-11-23 13:19:04 +0100

    Basic application files, sql file for database and database connection

---
+++ src/main/java/com/example/mindentudas/MindentudasApplication.java
@@ -0,0 +1,13 @@
+package com.example.mindentudas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindentudasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindentudasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mindentudas/MindentudasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mindentudas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindentudasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
